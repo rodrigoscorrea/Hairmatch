bf67ef3c568c66542163a5f7d8b0739d8ae53340
rodrigoscorrea <rodrigosc616@gmail.com>
2025-05-26 20:23:53 -0400

    fixed navigation data error, added WIP placeholder for unfinished pages, improved ai preferences supply

--- frontend-mobile/app/screens/customer/ProfileScreen.tsx
+++ frontend-mobile/app/screens/customer/ProfileScreen.tsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useContext } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BottomTabBar from "@/app/components/BottomBar";
 import { useBottomTab } from "@/app/contexts/BottomTabContext";
+import { styles } from "./styles/ProfileStyle";
 
 export default function ProfileScreen(){
     const { customer, setActiveTab } = useBottomTab();
 
+    const WIPImage = require("../../../assets/images/estamos-em-obras.jpg");
+
     useEffect(()=>{
-        setActiveTab('CustomerProfile');   
-    })
+        setActiveTab('CustomerProfile')
+    }, [])
 
     return (
-        <SafeAreaView>
-            <View>
-                <Text>CUSTOMER PROFILE SCREEN</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={{flex: 1}}>
+                <Text>Tela ainda não desenvolvida</Text>
+                <Image source={WIPImage} style={styles.wipImage}></Image>
             </View>
-            <BottomTabBar/>
+             <BottomTabBar />
         </SafeAreaView>
     )
 }
--- frontend-mobile/app/screens/customer/SearchScreen.tsx
+++ frontend-mobile/app/screens/customer/SearchScreen.tsx
@@ -1,15 +1,25 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, {useEffect} from "react";
+import { View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BottomTabBar from "@/app/components/BottomBar";
+import { styles } from "./styles/SearchStyle";
+import { useBottomTab } from "@/app/contexts/BottomTabContext";
 
 export default function SearchScreen(){
+    const { setActiveTab } = useBottomTab();
+    const WIPImage = require("../../../assets/images/estamos-em-obras.jpg");
+
+    useEffect(()=>{
+        setActiveTab('Search')
+    }, [])
+
     return (
-        <SafeAreaView>
-            <View>
-                <Text>SEARCH SCREEN</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={{flex: 1}}>
+                <Text>Tela ainda não desenvolvida</Text>
+                <Image source={WIPImage} style={styles.wipImage}></Image>
             </View>
-             <BottomTabBar activeTab="Search" />
+             <BottomTabBar />
         </SafeAreaView>
     )
 }
